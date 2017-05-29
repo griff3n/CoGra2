6f3550afa3ebcb527d7c79363e375a592562e185
pdick4 <peter.dick@fh-bielefeld.de>
2017-05-29 18:05:06 +0200

    change .gitignore
    Signed-off-by: pdick4 <peter.dick@fh-bielefeld.de>

--- Blatt03/Sphere.h
+++ Blatt03/Sphere.h
@@ -11,22 +11,19 @@
 
 #include "GLSLProgram.h"
 #include "Object.h"
+#include "DrawObject.h"
 
 namespace cg {
-	class Sphere {
+	class Sphere : DrawObject {
 	public:
 		// Create a wired sphere with the given color.
 		Sphere(const glm::vec3& color = glm::vec3(0.9f));
 		void render(const glm::mat4& view, const glm::mat4& projection);
 	private:
-		void initObject(const glm::vec3& color);
-		static void initShader(GLSLProgram& program, const std::string& vert, const std::string& frag);
+		void initObjectData(const glm::vec3& color);
 	public:
 		// Only public for easy access.
 		glm::mat4 model;
-	private:
-		GLSLProgram programSimple;
-		Object objSphere;
 	};
 } // namespace cg
 
